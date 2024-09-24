a8661a957dd7bad01cbdb26ff5d45934c202c940
adarshgupta404 <gadarsh747@gmail.com>
2024-09-24 15:26:59 +0530

    .

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ app.post("/generate-pdf", async (req, res) => {
         // "--single-process",
         "--no-zygote",
       ],
-      executablePath:
-        process.env.NODE_ENV === "production"
-          ? process.env.PUPPETEER_EXECUTABLE_PATH
-          : puppeteer.executablePath(),
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
+        // process.env.NODE_ENV === "production"
+        //   ? process.env.PUPPETEER_EXECUTABLE_PATH
+        //   : puppeteer.executablePath(),
     });
     console.log(4)
     const page = await browser.newPage();
