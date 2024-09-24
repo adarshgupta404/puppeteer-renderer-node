ae91dd0bdc320077d37fd70211f0463fd1d8fe4a
adarshgupta404 <gadarsh747@gmail.com>
2024-09-24 15:04:30 +0530

    .

--- index.js
+++ index.js
@@ -14,13 +14,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Route to convert HTML to PDF and return as buffer
 app.post("/generate-pdf", async (req, res) => {
   const { html } = req.body;
-
+  console.log(1)
   if (!html) {
     return res.status(400).send("HTML content is required");
   }
+  console.log(2)
 
   try {
     // Launch Puppeteer
+    console.log(3)
     const browser = await puppeteer.launch({
       args: [
         "--disable-setuid-sandbox",
@@ -33,15 +35,19 @@ app.post("/generate-pdf", async (req, res) => {
           ? process.env.PUPPETEER_EXECUTABLE_PATH
           : puppeteer.executablePath(),
     });
+    console.log(4)
     const page = await browser.newPage();
+    console.log(5)
     await page.setContent(html);
-
+    console.log(6)
     // Generate a unique file name for the PDF
     const pdfFileName = `generated_pdf_${uuidv4()}.pdf`;
     const pdfPath = path.join(__dirname, pdfFileName);
+    console.log(7)
     const scrollHeight = await page.evaluate(() => {
       return document.documentElement.scrollHeight;
     });
+    console.log(8)
 
     await page.pdf({
       path: pdfPath,
@@ -49,17 +55,18 @@ app.post("/generate-pdf", async (req, res) => {
       height: `${scrollHeight + 60}px`, // Add 60px to the calculated scroll height
       printBackground: true, // Ensure background is printed
     });
-
+    console.log(9)
     await browser.close();
 
     // Read the PDF file into a buffer
     const pdfBuffer = fs.readFileSync(pdfPath);
-
+    console.log(10)
     // Optionally, delete the PDF file after reading
     fs.unlinkSync(pdfPath);
-
+    console.log(11)
     // Send the PDF buffer in the response
     res.setHeader("Content-Type", "application/pdf");
+    console.log(12)
     res.send(pdfBuffer);
   } catch (error) {
     console.error("Error generating PDF:", error);
